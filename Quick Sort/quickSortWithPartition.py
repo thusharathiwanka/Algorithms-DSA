8c5b6213cf698066dd2c5bda259f92bb661bb461
thusharathiwanka <thusharathiwanka123@gmail.com>
2020-10-12 23:09:30 +0530

    ADD: quick sort with partition added

---
+++ Quick Sort/quickSortWithPartition.py
@@ -0,0 +1,34 @@
+array = [4, 2, 1, 6, 3]
+n = len(array)
+p = 0
+r = n - 1
+
+
+def partition(array, p, r):
+    x = array[r]
+    i = p - 1
+
+    for j in range(p, r):
+        if(array[j] <= x):
+            i = i + 1
+            temp1 = array[i]
+            array[i] = array[j]
+            array[j] = temp1
+
+    temp2 = array[i + 1]
+    array[i + 1] = array[r]
+    array[r] = temp2
+
+    return i + 1
+
+
+def quickSort(array, p, r):
+    if(p <= r):
+        q = partition(array, p, r)
+        quickSort(array, p, q - 1)
+        quickSort(array, q + 1, r)
+
+    return array
+
+
+print(quickSort(array, p, r))
