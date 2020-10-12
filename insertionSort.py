4e0bf7ce49b145b159b0166b78bfb8a1e7437287
thusharathiwanka <thusharathiwanka123@gmail.com>
2020-10-12 17:22:56 +0530

    ADD: insertion sort added

---
+++ insertionSort.py
@@ -0,0 +1,15 @@
+def insertionSort(array):
+    for j in range(1, len(array)):
+        key = array[j]
+        i = j - 1
+
+        while i >= 0 and array[i] > key:
+            array[i + 1] = array[i]
+            i = i - 1
+
+        array[i + 1] = key
+
+    return array
+
+
+print(insertionSort([4, 2, 1, 6, 3]))
