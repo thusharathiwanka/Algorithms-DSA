71b2c3f94b23ddf502f00619b77302ff35383fa2
thusharathiwanka <thusharathiwanka123@gmail.com>
2020-10-12 23:31:47 +0530

    ADD: selection sort added

---
+++ Selection Sort/selectionSort.py
@@ -0,0 +1,17 @@
+def selectionSort(array):
+    indexing_length = range(0, len(array) - 1)
+
+    for i in indexing_length:
+        min_value = i
+
+        for j in range(i+1, len(array)):
+            if array[j] < array[min_value]:
+                min_value = j
+
+            if min_value != i:
+                array[min_value], array[i] = array[i], array[min_value]
+
+    return array
+
+
+print(selectionSort([4, 2, 1, 6, 3]))
